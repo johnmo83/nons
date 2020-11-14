51b44770f426377aafc1829d1ec2ec983f7b931f
johnmo83 <54600003+johnmo83@users.noreply.github.com>
2020-11-13 20:37:26 -0500

    Create getbam.sh

---
+++ getbam.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --job-name=j_getbam
+#SBATCH --partition=highmem_p
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=username@uga.edu
+#SBATCH --ntasks=1
+#SBATCH --mem=10gb
+#SBATCH --time=08:00:00
+#SBATCH --output=PAML.%j.out
+#SBATCH --error=PAML.%j.err
